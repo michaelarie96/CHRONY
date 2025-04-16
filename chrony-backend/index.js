5026eb695cc1a3e72934107bc03f23fe099fd541
dorzzz <doradiv12@gmail.com>
2025-04-16 15:09:30 +0300

    New Commit

--- chrony-backend/index.js
+++ chrony-backend/index.js
@@ -1,22 +1,30 @@
+// libraries modules
 const express = require('express');
 const mongoose = require('mongoose')
+require('dotenv').config();
+
+// user-created modules
+const User = require('./models/user');
+
+
+
 const app = express();
 const PORT = 3000;
 
 
-mongoose.connect('mongodb+srv//dorz:Doradiv10@dordb.wz8qgma.mongodb.net/?retryWrites=true&w=majority&appName=DorDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('MongoDB connection error:', err));
+//mongoose.connect(process.env.mongoURI).then(() => console.log("connected to Database"))
+//.catch(err => console.error('MongoDB connection error:', err));
 
 
-//mongoose.connect(`mongodb+srv//dorz:Doradiv10@dordb.wz8qgma.mongodb.net/?retryWrites=true&w=majority&appName=DorDB`);
 
 app.get('/', (req, res) => {
     res.send('Backend is working!');
 });
+
+app.get('/api/ping', (req, res) => {
+    console.log('Frontend hit the backend at /api/ping');
+    res.json({ message: 'Pong from backend' });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
