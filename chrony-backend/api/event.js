b5b4e43630160694255c0f087d60fca23c4961cc
Michael Arie <51443126+michaelarie96@users.noreply.github.com>
2025-04-25 16:05:10 +0300

    test saving events in the DB

--- chrony-backend/api/event.js
+++ chrony-backend/api/event.js
@@ -6,7 +6,7 @@ const Event = require('../models/event');
 router.post('/', async (req, res) => {
   try {
     const event = new Event(req.body);
-    //await event.save();
+    await event.save();
     res.status(201).json(event);
   } catch (error) {
     res.status(400).json({ error: error.message });
